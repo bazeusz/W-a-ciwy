14d67c064ba1c116d9c3ba4e39cba9decd684058
FP\mbazydlo <mbazydlo@future-processing.com>
2017-02-16 15:29:13 +0100

    Change 4

---
+++ Właściwy/Controllers/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Właściwy.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult Board()
+        {
+            return View();
+        }
+    }
+}
