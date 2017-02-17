24c5880d4260c7427225d87e0d1fe3bce87d939e
FP\mbazydlo <mbazydlo@future-processing.com>
2017-02-17 09:54:55 +0100

    New views anc controllers

---
+++ Właściwy/Controllers/TipsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Właściwy.Controllers
+{
+    public class TipsController : Controller
+    {
+        // GET: Tips
+        public ActionResult Tips()
+        {
+            return View();
+        }
+    }
+}
